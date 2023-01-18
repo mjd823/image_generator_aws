1b05fc4af1faeb12e47e40c67ed65169ceffce7f
mjd823 <mjdawson821@gmail.com>
2023-01-18 11:35:29 -0600

    configuration

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const path = require('path');
+const fetch = require('node-fetch');
 const { generateImage } = require('./controllers/openaiController');
 
 const app = express();
 
 // Enable body parser
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/generateimage', async (req, res) => {
+    const {prompt} = req.body;
 
-app.use('/openai', require('./routes/openaiRoutes'));
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({prompt, n:3}),
+    };
 
-const serverless = require('serverless-http');
-const handler = serverless(app);
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', options);
+        const json = await response.json();
 
-module.exports.app = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+        res.status(200).json({
+            success: true,
+            data: json.data.map(image => image.url)
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+module.exports.app = async (event, context, callback) => {
+    const handler = serverless(app);
     return handler(event, context, callback);
 };
-
-
-//test
