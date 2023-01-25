989c5fd135b8b2799584616ae1bcf8d419e40819
mjd823 <mjdawson821@gmail.com>
2023-01-25 09:03:36 -0600

    config 3

--- amplify/backend/function/generateimagelamb/src/index.js
+++ amplify/backend/function/generateimagelamb/src/index.js
@@ -1,17 +1,54 @@
+const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+const generateImage = async (event, context, callback) => {
+    const {prompt} = JSON.parse(event.body);
+
+    const imageSize =  '512x512'
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n : 3,
+            size : imageSize
+        });
+
+        const imageUrls = [response.data.data[0].url, response.data.data[1].url, response.data.data[2].url];
+
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
+            body: JSON.stringify({
+                success : true,
+                data : imageUrls
+            })
+        });
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+        callback(null, {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
+            body: JSON.stringify({
+                success : false,
+                error : 'image could not be generated'
+            })
+        });
+    }
 };
+
+exports.handler = generateImage;
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api17f026f1": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "generateimagelamb": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
